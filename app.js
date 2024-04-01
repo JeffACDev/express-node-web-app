1d1b2419bd22d5f93e9115c1f3b4ea871d7e4626
JEFFERY ANDERSON <jefferyanderson@JEFFERYs-MacBook-Pro.local>
2024-04-01 08:36:03 -0500

    single item page

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-const express = require('express');
+const express = require("express");
 // const chalk = require('chalk'); // TODO: fix ES Module Error
-const debug = require('debug')('app');
-const morgan = require('morgan');
-const path = require('path');
+const debug = require("debug")("app");
+const morgan = require("morgan");
+const path = require("path");
+const sessions = require("./src/data/sessions.json");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 const sessionsRouter = express.Router();
 
-app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname, '/public/')));
+app.use(morgan("tiny"));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.set('views', './src/views');
-app.set('view engine', 'ejs');
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
 
-sessionsRouter.route('/').get((req, res)=>{
-    res.render("sessions", {
-      sessions: [
-        { title: "Session 1", description: "this is session 1" },
-        { title: "Session 2", description: "this is session 2" },
-        { title: "Session 3", description: "this is session 3" },
-        { title: "Session 4", description: "this is session 4" },
-      ],
-    });
+sessionsRouter.route("/").get((req, res) => {
+  res.render("sessions", {
+    sessions,
+  });
 });
 
-sessionsRouter.route('/1/').get((req, res)=>{
-    res.send('hello single sessions');
+sessionsRouter.route("/:id/").get((req, res) => {
+  const id = req.params.id;
+  res.render("session", {
+    session: sessions[id],
+  });
 });
 
-app.use('/sessions', sessionsRouter);
+app.use("/sessions", sessionsRouter);
 
-app.get('/',(req, res)=>{
-    res.render('index',{title:'Anderson Creative Design', data:['a','b','c']});
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Anderson Creative Design",
+    data: ["a", "b", "c"],
+  });
 });
 
-app.listen(PORT, ()=>{
-    debug(`listening to port ${PORT}`)
+app.listen(PORT, () => {
+  debug(`listening to port ${PORT}`);
 });
-
